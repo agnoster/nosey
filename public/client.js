00876264394f55601dc8257ea74c9fb5dbdd9c4c
Isaac Wolkerstorfer <agnoster@gmail.com>
2011-06-02 13:27:06 +0200

    Basic display of test results

--- public/client.js
+++ public/client.js
@@ -2,5 +2,16 @@ var socket = new io.Socket()
 
 socket.connect()
 socket.on('message', function(msg) {
-  console.log(msg)
+  var msg = msg.split(':')
+    , name = msg[0]
+    , status = msg[1]
+  var el = document.getElementById(name)
+
+  if (!el) {
+    el = document.createElement('div')
+    el.setAttribute('id', name)
+    el.innerHTML = name
+    document.body.appendChild(el)
+  }
+  el.setAttribute('class', status)
 })
