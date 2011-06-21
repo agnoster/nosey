667e61f756b62179e9f046be424bcbe4eea37ab1
Isaac Wolkerstorfer <agnoster@gmail.com>
2011-06-21 13:59:53 +0200

    Split out ansi2html

--- test/ansi2html_spec.js
+++ test/ansi2html_spec.js
@@ -2,14 +2,29 @@ var ansi2html = require('../lib/ansi2html')
   , vows = require('vows')
   , assert = require('assert')
 
-vows.describe('ansi2html').addBatch(
-{ 'when loading ansi2html':
-  { topic: function() { return ansi2html }
-  , 'it should exist': function(it) { assert.notEqual(it, null) }
+function makeBatchFromTestCases(testCases) {
+  function makeTest(input, expected) {
+    var test = { topic: function() { return ansi2html(input) } }
+    test['we get "' + expected + '"'] = function(topic) { assert.equal(topic, expected) }
+    return test
   }
-, 'when encoding a string':
-  { topic: function() { return ansi2html('test string') }
-  , 'it should be correct': function(it) { assert.equal(it, 'test string') }
+
+  var batch = {}
+  for (var key in testCases) {
+    if (!testCases.hasOwnProperty(key)) continue
+    batch['When encoding "' + key + '"'] = makeTest(key, testCases[key])
   }
-}).export(module)
+  return batch
+}
+
+var suite = vows.describe('ansi2html')
+suite.addBatch(makeBatchFromTestCases(
+{ 'testString': 'testString'
+, 'test[0mString': 'testString'
+, 'test[32mString': 'test<span style="color:green">String</span>'
+, 'test[1mBold[32mGreen[31mRed[0mNone': 'test<span style="font-weight:bold">Bold</span><span style="font-weight:bold;color:green">Green</span><span style="font-weight:bold;color:red">Red</span>None'
+, 'test[31mRed[1mBold[39mString': 'test<span style="color:red">Red</span><span style="color:red;font-weight:bold">Bold</span><span style="font-weight:bold">String</span>'
+, 'test[42mGreenBG': 'test<span style="background:green">GreenBG</span>'
+}))
+suite.export(module)
 
